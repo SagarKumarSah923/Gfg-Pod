be683813515ea74d619917f9728b8d0d7877b9b3
Sagar kumar Sah <152137743+SagarKumarSah923@users.noreply.github.com>
2024-03-09 18:17:40 +0530

    Add files via upload
    You are given an array arr[] of length n, you have to re-construct the same array arr[] in-place. The arr[i] after reconstruction will become arr[i] OR arr[i+1], where OR is bitwise or. If for some i, i+1 does not exists, then do not change arr[i].
    
    Example 1:
    
    Input :
    n = 5
    arr[] = {10, 11, 1, 2, 3}
    Output :
    11 11 3 3 3
    Explanation:
    At index 0, arr[0] or arr[1] = 11
    At index 1, arr[1] or arr[2] = 11
    At index 2, arr[2] or arr[3] = 3
    ...
    At index 4, No element is left So, it will remain as it is.
    New Array will be {11, 11, 3, 3, 3}.

---
+++ Play with or.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int * game_with_number(int arr[],int n){
+	for(int i=0;i<n-1;i++){
+		arr[i]=arr[i]|arr[i+2];
+	}
+	return arr;
+}
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		int arr[n];
+		
+		for(int i=0;i<n;i++)
+		    cin>>arr[i];
+		    
+		int *arr2;
+		
+		arr2 =game_with_number(arr,n);
+		for(int i=0;i<n;i++)
+		    cout<<arr2[i] <<" ";
+		    
+		cout<<endl;
+	}
+}
+
